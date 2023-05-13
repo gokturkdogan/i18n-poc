8b2b4252c8127cb3ac4330feffcf2d77bb8b3c57
gokturk.dogan <gokturk.dogan@hotmail.com>
2023-05-13 21:53:53 +0300

    local messaged move to json

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,40 +1,14 @@
 import { createI18n } from "vue-i18n"
-
-const messages = {
-    en: {
-        nav: {
-            home: "Home",
-            about: "About"
-        },
-        home: {
-            header: "Welcome to the Vue 3 I18n POC development",
-            created_by: "This POC was developed by {developer}.",
-        },
-        about: {
-            header: "About us",
-            desc: "{developer} is a Junior Frontend Developer."
-        }
-    },
-    tr: {
-        nav: {
-            home: "Anasayfa",
-            about: "Hakkında"
-        },
-        home: {
-            header: "Vue 3 I18n POC geliştirmesine hoşgeldiniz",
-            created_by: "Bu POC çalışması {developer} tarafından geliştirilmiştir",
-        },
-        about: {
-            header: "Hakkında",
-            desc: "{developer} Junior bir Frontend Developerdır."
-        }
-    }
-}
+import en from "./locales/en.json"
+import tr from "./locales/tr.json"
 
 export default createI18n({
     locale: import.meta.env.VITE_DEFAULT_LOCALE,
     fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
     legacy: false,
     globalInjection: true,
-    messages
+    messages: {
+        en,
+        tr
+    }
 })
